ac15dd8375f785e694244f0b8c5bec53be077376
jmp9292 <josema92@gmail.com>
2015-10-28 17:42:18 +0100

    Inicio de repositorio de proyectos Android

---
+++ Splash/app/src/main/java/com/example/josemanuel/splash/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.example.josemanuel.splash;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by josemanuel on 27/10/2015.
+ */
+public class SplashScreen extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
